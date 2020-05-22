ddbf23c5d95d31baff6a7fdb736f317ab9b42fec
noxi515 <tzo.ppl@gmail.com>
2020-05-22 16:39:23 +0900

    Add divider column and excel export

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 import { CollectionView } from '@grapecity/wijmo';
 import { AllowMerging, Column, FlexGrid, HeadersVisibility, Row } from '@grapecity/wijmo.grid';
+import { FlexGridXlsxConverter } from '@grapecity/wijmo.grid.xlsx';
 
 type Item = {
   prop0: string,
   prop1: string,
+
   prop2: string,
   prop3: string,
   prop4: string,
   prop5: string,
+
   prop6: string,
   prop7: string,
   prop8: string,
+  prop9: string,
 };
 
 const WjEnums = {
@@ -35,14 +39,15 @@ export class AppComponent {
     row1.allowMerging = true;
     row1.dataItem = {
       prop0: 'prop 0',
-      prop1: 'AAA',
+      prop1: 'prop 1',
       prop2: 'AAA',
       prop3: 'AAA',
       prop4: 'AAA',
-      prop5: 'BBB',
+      prop5: 'AAA',
       prop6: 'BBB',
       prop7: 'BBB',
       prop8: 'BBB',
+      prop9: 'BBB',
     } as Item;
     grid.columnHeaders.rows.splice(0, 0, row1);
 
@@ -50,46 +55,51 @@ export class AppComponent {
     row2.allowMerging = true;
     row2.dataItem = {
       prop0: 'prop 0',
-      prop1: 'aaa',
-      prop2: 'aaa',
-      prop3: 'bbb',
-      prop4: 'bbb',
-      prop5: 'ccc',
-      prop6: 'ccc',
-      prop7: 'ddd',
-      prop8: 'ddd',
+      prop1: 'prop 1',
+      prop2: 'AAA',
+      prop3: 'AAA',
+      prop4: 'AAA',
+      prop5: 'AAA',
+      prop6: 'BBB',
+      prop7: 'BBB',
+      prop8: 'BBB',
+      prop9: 'BBB',
     } as Item;
     grid.columnHeaders.rows.splice(1, 0, row2);
 
     // 行の中からCSSクラスが付いている要素を取得して、最初と最後にCSSクラスを付与する
-    grid.updatedView.addHandler((sender, args) => {
-      const rows = sender.hostElement.querySelectorAll('.wj-row');
-      for (const row of Array.from(rows)) {
-        for (const str of ['AAA', 'BBB']) {
-          const targetElements = row.querySelectorAll('.' + str);
-          if (targetElements.length === 0) {
-            continue;
-          }
-
-          for (let i = 0; i < targetElements.length; i++) {
-            if (i === 0) {
-              targetElements[i].classList.add('border-left');
-            } else {
-              targetElements[i].classList.remove('border-left');
-            }
-
-            if (i === targetElements.length - 1) {
-              targetElements[i].classList.add('border-right');
-            } else {
-              targetElements[i].classList.remove('border-right');
-            }
-          }
-        }
-      }
-    });
+    // grid.updatedView.addHandler((sender, args) => {
+    //   const rows = sender.hostElement.querySelectorAll('.wj-row');
+    //   for (const row of Array.from(rows)) {
+    //     for (const str of ['AAA', 'BBB']) {
+    //       const targetElements = row.querySelectorAll('.' + str);
+    //       if (targetElements.length === 0) {
+    //         continue;
+    //       }
+    //
+    //       for (let i = 0; i < targetElements.length; i++) {
+    //         if (i === 0) {
+    //           targetElements[i].classList.add('border-left');
+    //         } else {
+    //           targetElements[i].classList.remove('border-left');
+    //         }
+    //
+    //         if (i === targetElements.length - 1) {
+    //           targetElements[i].classList.add('border-right');
+    //         } else {
+    //           targetElements[i].classList.remove('border-right');
+    //         }
+    //       }
+    //     }
+    //   }
+    // });
   }
 
   trackByColumn(index: number, column: Column) {
     return column.binding;
+  }
+
+  exportExcel(grid: FlexGrid) {
+    FlexGridXlsxConverter.saveAsync(grid, {}, 'test.xlsx');
   }
 }
